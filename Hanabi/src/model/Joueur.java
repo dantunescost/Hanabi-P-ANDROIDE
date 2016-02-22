7ad9dc21d792c6d523f244339a886d6289d7e1e3
Keithdae <keithdae@gmail.com>
2016-02-22 15:10:19 +0100

    Constructeur joueur dans la superclasse

--- Hanabi/src/model/Joueur.java
+++ Hanabi/src/model/Joueur.java
@@ -4,6 +4,11 @@ public abstract class Joueur {
 	protected Main main;
 	protected String nom; 
 	
+	public Joueur(String nom,int nbCartes){
+		this.nom = nom;
+		this.main = new Main(nbCartes);
+	}
+	
 	public Main getMain(){
 		return this.main;
 	}
--- Hanabi/src/model/JoueurHumain.java
+++ Hanabi/src/model/JoueurHumain.java
@@ -3,11 +3,7 @@ package model;
 public class JoueurHumain extends Joueur {
 	
 	public JoueurHumain(String nom,int nbCartes){
-		this.nom = nom;
-		this.main = new Main(nbCartes);
+		super(nom,nbCartes);
 	}
 	
-	public void setMain(int nbCartes, Carte[] cartes){
-		this.main = new Main(nbCartes, cartes);
-	}
 }
