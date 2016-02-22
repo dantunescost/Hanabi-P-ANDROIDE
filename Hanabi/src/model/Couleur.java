79f185b5b3f9f6c8c82848989e2371d7ccfab2ef
Keithdae <keithdae@gmail.com>
2016-02-22 16:40:15 +0100

    Ajout de javadoc pour la classe Couleur

--- Hanabi/src/model/Couleur.java
+++ Hanabi/src/model/Couleur.java
@@ -1,17 +1,40 @@
 package model;
 
+/**
+ * Represente les couleurs des cartes.
+ */
 public class Couleur {
+	/**
+	 * Enumeration des couleurs possibles
+	 */
 	public enum CardColor{BLANC, ROUGE, BLEU, VERT, JAUNE, MULTI}
+	/**
+	 * Contient la valeur de cette couleur
+	 */
 	private CardColor couleur;
 	
+	/**
+	 * Constructeur pour une couleur
+	 * @param c	La couleur voulue
+	 */
 	public Couleur(CardColor c){
 		this.couleur = c;
 	}
-
+	
+	
+	/**
+	 * @return	La couleur de cette couleur
+	 */
 	public CardColor getCouleur() {
 		return couleur;
 	}
 	
+	
+	/**
+	 * Effectue la conversion entre une chaine de caractere et la couleur correspondante
+	 * @param c	La chaine a convertir
+	 * @return	La couleur obtenue par cette conversion
+	 */
 	public static CardColor convertirEnCouleur(String c){
 		CardColor couleur = null;
 		if(c.toUpperCase().equals("BLANC") || c.toUpperCase().equals("WHITE") || c.toUpperCase().equals("W")){
