c75c26c0eac60ff1c79c3ba47b9066692f92cadf
Antunes Costa Goncalves Daniel <dantunescost92@gmail.com>
2016-03-09 08:02:31 +0100

    interface graphique

--- Hanabi/src/view/FenetrePartie.java
+++ Hanabi/src/view/FenetrePartie.java
@@ -1,44 +1,87 @@
 package view;
 
+import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+
+import model.AdditionMainPleineException;
+import model.Partie;
+import model.PiocheVideException;
 
 public class FenetrePartie extends JFrame{
 	private static final long serialVersionUID = 2656325461540137440L;
-	private Table table = new Table();
+	private Table table;
+	private Partie partie; 
 
-	public FenetrePartie(){
+	public FenetrePartie(Partie p){
 		super("Hanabi");
+		this.partie = p;
 		this.setSize(1000, 600);
-		this.setResizable(false);
+		this.setResizable(true);
 		
-		ImageIcon woodBG = new ImageIcon("ressources/wood.jpg");
-		JLabel background=new JLabel(new ImageIcon(woodBG.getImage().getScaledInstance(1000, 600, Image.SCALE_SMOOTH)));
-			
-		JLayeredPane lp = new JLayeredPane();
-		lp.add(background, JLayeredPane.DEFAULT_LAYER);
-		lp.add(this.table, JLayeredPane.PALETTE_LAYER);
+		this.table = new Table();
 		
-		this.setContentPane(lp);
+		JPanel bg = new JPanel();/*{
+			private static final long serialVersionUID = 1L;
+
+			public void paintComponent(Graphics g)
+			{
+				super.paintComponent(g);
+				g.clearRect(0,0,this.getWidth(),this.getHeight());
+				g.drawImage(new ImageIcon("ressources/wood.jpg").getImage(), 0, 0, 1000, 600, this);
+				
+			}
+		};*/
+		bg.setLayout(new FlowLayout());
+		bg.add(table);
+		this.setContentPane(bg);
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
 	}
 	
-	public static void main(String[] args){
-		new FenetrePartie();
+	public void afficherMain(Graphics g){
+		int width = 692, height = 365;
+		int karteH = height/4;
+		int karteW =(int)( (float)karteH*0.645);
+		int startX = (this.getWidth() - width) /2 + width/2;
+		int startY = (this.getHeight() - height) /2;
+		Image karte = new ImageIcon("ressources/karte.jpg").getImage();
+		g.drawImage(karte, startX, startY-(karteH/3)*2, karteW, karteH, this);
 	}
 	
 	public void paint(Graphics g){
-		super.paint(g);
+		//super.paint(g);
 		g.clearRect(0,0,this.getWidth(),this.getHeight());
 		g.drawImage(new ImageIcon("ressources/wood.jpg").getImage(), 0, 0, this.getWidth(), this.getHeight(), this);
-		this.paintComponents(g);
+		this.table.paintTable(g, this);
+		afficherMain(g);
+	}
+	
+	public Partie getPartie() {
+		return partie;
+	}
+
+	/************************* MAIN *************************/
+	public static void main(String[] args){
+		Partie game = new Partie(2,8,false);
+	    String[] noms = new String[2];
+	    noms[0] = "Holmes";
+	    noms[1] = "Watson";
+	    try {
+			game.initPartie(noms);
+		} catch (AdditionMainPleineException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		} catch (PiocheVideException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+	    new FenetrePartie(game);
 	}
 }
--- Hanabi/src/view/Table.java
+++ Hanabi/src/view/Table.java
@@ -8,9 +8,11 @@ import javax.swing.JPanel;
 public class Table extends JPanel {
 	private static final long serialVersionUID = 1L;
 
-	public void paint(Graphics g)
+	public void paintTable(Graphics g, FenetrePartie fen)
 	{
-		g.clearRect(0,0,this.getWidth(),this.getHeight());
-		g.drawImage(new ImageIcon("ressources/table.png").getImage(), 0, 0, 692, 365, this);
+		int width = 692, height = 365;
+		int startX = (fen.getWidth() - width) /2;
+		int startY = (fen.getHeight() - height) /2;
+		g.drawImage(new ImageIcon("ressources/table.png").getImage(), startX, startY, width, height, this);
 	}
 }
