5d43b084b87fd996d99f747aee5a519db358a329
BufferOverfl0w <cedric.hubert13@gmail.com>
2016-04-29 10:31:36 +0200

    heruisticIA

---
+++ Hanabi/src/model/HeuristicJoueurIA.java
@@ -0,0 +1,401 @@
+package model;
+
+public class HeuristicJoueurIA extends JoueurIA{
+
+	
+	
+	public HeuristicJoueurIA(String nom, Partie p, int id) 
+	{
+        super(nom, p, id);
+    }
+	public void jouerCoup()
+	{
+		int i=0;
+		double h_max=0;
+		double h_jouer[]=new double[this.getMain().main.size()];
+		double h_defausser[]=new double[this.getMain().main.size()];
+		double h_indice_couleur[]=new double[(this.p.nbJoueurs)*5];
+		double h_indice_valeur[]=new double[(this.p.nbJoueurs)*5];
+		/*if(this.p.multicolor)
+			h_indice_couleur=new double[(this.p.nbJoueurs)*5];
+		else if(!this.p.multicolor)
+			h_indice_couleur=new double[(this.p.nbJoueurs)*6];
+		*/
+		/*********Tests jouer************/
+		i=0;
+		for(Carte c : this.getMain().main)
+		{
+			h_jouer[i]=testJouerCarte(c);
+			i++;
+		}
+		
+		/*********Tests defausser************/
+		i=0;
+		for(Carte c : this.getMain().main)
+		{
+			h_defausser[i]=testDefausserCarte(c);
+			i++;
+		}
+		
+		/*********Tests indices************/
+		for(Joueur j : p.joueurs)
+    	{
+    		if (j.getId()!=this.id)
+    		{
+    			h_indice_valeur[j.getId()]=testIndice(j, null, 1);
+    			h_indice_valeur[j.getId()+1]=testIndice(j, null, 2);
+    			h_indice_valeur[j.getId()+2]=testIndice(j, null, 3);
+    			h_indice_valeur[j.getId()+3]=testIndice(j, null, 4);
+    			h_indice_valeur[j.getId()+4]=testIndice(j, null, 5);
+    			h_indice_couleur[j.getId()]=testIndice(j, Couleur.CardColor.BLANC, 0);
+    			h_indice_couleur[j.getId()+1]=testIndice(j, Couleur.CardColor.BLEU, 0);
+    			h_indice_couleur[j.getId()+2]=testIndice(j, Couleur.CardColor.VERT, 0);
+    			h_indice_couleur[j.getId()+3]=testIndice(j, Couleur.CardColor.JAUNE, 0);
+    			h_indice_couleur[j.getId()+4]=testIndice(j, Couleur.CardColor.ROUGE, 0);
+    		}
+    		
+    	}
+		/*********Recherche h max**********/
+		for(i=0; i<this.getMain().main.size();i++)
+		{
+			if(h_jouer[i]>h_max)
+				h_max=h_jouer[i];
+			if(h_defausser[i]>h_max)
+				h_max=h_defausser[i];
+		}
+		for(Joueur j : p.joueurs)
+    	{
+			for(i=0; i<5;i++)
+			{
+				if(h_indice_valeur[j.getId()+i]>h_max)
+					h_max=h_indice_valeur[j.getId()+i];
+				if(h_indice_couleur[j.getId()+i]>h_max)
+					h_max=h_indice_couleur[j.getId()+i];
+			}
+    	}
+		//selection coup
+		boolean a_joue=false;
+		for(Joueur j : p.joueurs)
+    	{
+			for(i=0; i<5;i++)
+			{
+				if(a_joue==false)
+				{
+					if(h_indice_valeur[j.getId()+i]==h_max)
+					{
+						try {
+							p.indiceValeur(j, i+1);
+						} catch (IndiceSoitMemeException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						a_joue=true;
+					}
+				}
+				if(a_joue==false)
+				{
+					if(h_indice_couleur[j.getId()+i]==h_max)
+					{
+						Couleur.CardColor coul=null;
+						if(i==0)
+							coul=Couleur.CardColor.BLANC;
+						else if(i==1)
+							coul=Couleur.CardColor.BLEU;
+						else if(i==2)
+							coul=Couleur.CardColor.VERT;
+						else if(i==3)
+							coul=Couleur.CardColor.JAUNE;
+						else if(i==4)
+							coul=Couleur.CardColor.ROUGE;
+						System.out.println(i);
+						System.out.println(coul.toString());
+						try {
+							p.indiceCouleur(j, coul);
+						} catch (IndiceSoitMemeException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						a_joue=true;
+					}
+				}
+			}
+    	}
+		for(i=0; i<this.getMain().main.size();i++)
+		{
+			if(a_joue==false)
+			{
+				if(h_jouer[i]==h_max)
+				{
+					try {
+						p.joueCarte(this, i);
+					} catch (EnleverCarteInexistanteException | PartiePerdueException | AdditionMainPleineException
+							| PiocheVideException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					a_joue=true;
+				}
+			}
+			if(a_joue==false)
+			{
+				if(h_defausser[i]==h_max)
+				{
+					try {
+						p.defausse(this, i);
+					} catch (EnleverCarteInexistanteException | AdditionMainPleineException
+							| PiocheVideException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					a_joue=true;
+				}
+			}
+		}
+	}
+	public double testJouerCarte(Carte c1)
+	{
+		double h=0;
+		PartieView pw=this.p.createView();
+		pw.majJoueur(this);
+		pw.decr_nb_jouables();//la carte que l'on va jouer
+		pw.incr_nb_points();
+		if(this.isJouableTrivial(c1))
+		{
+			for(Carte c2 : this.getMain().main)
+			{
+				if((c2.isValeurConnue())&&(c2.isCouleurConnue()))
+				{
+					if(c2!=c1)
+					{
+						if(c2.getCouleur()==c1.getCouleur())
+						{
+							if(c2.getValeur()==c1.getValeur())
+							{
+								pw.incr_nb_defaussables();
+								pw.incr_info_defaussables();
+								pw.incr_info_defaussables();
+								pw.decr_nb_jouables();
+								pw.decr_info_jouables();
+								pw.decr_info_jouables();
+							}
+							else if(c2.getValeur()==(c1.getValeur()+1))
+							{
+								pw.incr_nb_jouables();
+								pw.incr_info_jouables();
+								pw.incr_info_jouables();
+								pw.decr_info_neutres();
+								pw.decr_info_neutres();
+							}
+						}
+					}
+				}
+			}
+			for(Joueur j : p.joueurs)
+	    	{
+	    		if (j.getId()!=this.id)
+	    		{
+	    			for(Carte c3 : j.getMain().main)
+	    			{
+	    				if(c3.getCouleur()==c1.getCouleur())
+						{
+							if(c3.getValeur()==c1.getValeur())
+							{
+								pw.incr_nb_defaussables();
+								pw.decr_nb_jouables();
+								if(c3.isCouleurConnue())
+								{
+									pw.incr_info_defaussables();
+									pw.decr_info_jouables();
+								}
+								if(c3.isValeurConnue())
+								{
+									pw.incr_info_defaussables();
+									pw.decr_info_jouables();
+								}	
+							}
+							else if(c3.getValeur()==(c1.getValeur()+1))
+							{
+								pw.incr_nb_jouables();
+								if(c3.isCouleurConnue())
+								{
+									pw.incr_info_jouables();
+									pw.decr_info_neutres();
+								}
+								if(c3.isValeurConnue())
+								{
+									pw.incr_info_jouables();
+									pw.decr_info_neutres();
+								}
+							}
+						}
+	    			}
+	    		}
+	    	}
+			h=pw.valeur_heuristique();
+		}
+		return h;
+	}
+	public double testDefausserCarte(Carte c1)
+	{
+		PartieView pw=this.p.createView();
+		pw.majJoueur(this);
+		pw.incr_nb_indices();//puisque l'on defausse une carte
+		boolean bloquage=false;
+		int nb_meme_carte_defausse=0;
+		for(Carte c3 : this.p.getDefausse())
+		{
+			if((c3.getCouleur()==c1.getCouleur())&&(c3.getValeur()==c1.getValeur()))
+				nb_meme_carte_defausse++;
+		}
+		if(c1.getValeur()==1)
+		{
+			if(nb_meme_carte_defausse==2)
+				bloquage=true;
+		}
+		else if(c1.getValeur()!=5)
+		{
+			if(nb_meme_carte_defausse==1)
+				bloquage=true;
+		}
+		
+		//Donnees directes
+		if(this.isJouableTrivial(c1))
+		{
+			pw.decr_nb_jouables();
+			if(c1.isCouleurConnue())
+			{
+				pw.decr_info_jouables();
+			}
+			if(c1.isValeurConnue())
+			{
+				pw.decr_info_jouables();
+			}
+		}
+		else if(this.isDefaussableTrivial(c1))
+		{
+			if(c1.isCouleurConnue())
+			{
+				pw.decr_info_defaussables();
+			}
+			if(c1.isValeurConnue())
+			{
+				pw.decr_info_defaussables();
+			}
+		}
+		else
+		{
+			if(c1.isCouleurConnue())
+			{
+				pw.decr_info_neutres();
+			}
+			if(c1.isValeurConnue())
+			{
+				pw.decr_info_neutres();
+			}
+		}
+		//Parcours des mains et donnees liees
+		for(Carte c2 : this.getMain().main)
+		{
+			if((c2.isValeurConnue())&&(c2.isCouleurConnue()))
+			{
+				if(c2!=c1)
+				{
+					if(c2.getCouleur()==c1.getCouleur())
+					{
+						if(c2.getValeur()>(c1.getValeur()))
+						{
+							if(bloquage)
+							{
+								pw.incr_nb_defaussables();
+								pw.incr_info_defaussables();
+								pw.incr_info_defaussables();
+								pw.decr_info_neutres();
+								pw.decr_info_neutres();
+							}
+						}
+					}
+				}
+			}
+		}
+		for(Joueur j : p.joueurs)
+    	{
+    		if (j.getId()!=this.id)
+    		{
+    			for(Carte c3 : j.getMain().main)
+    			{
+    				if(c3.getCouleur()==c1.getCouleur())
+					{
+    					if(c3.getValeur()>(c1.getValeur()))
+						{
+							if(bloquage)
+							{
+								pw.incr_nb_defaussables();
+								if(c3.isCouleurConnue())
+								{
+									pw.incr_info_defaussables();
+									pw.decr_info_neutres();
+								}
+								if(c3.isValeurConnue())
+								{
+									pw.incr_info_defaussables();
+									pw.decr_info_neutres();
+								}
+							}
+						}
+					}
+    			}
+    		}
+    	}
+		return pw.valeur_heuristique();
+	}
+	public double testIndice(Joueur j, Couleur.CardColor couleur, int valeur)//laisser un des 2 à null/0
+	{
+		PartieView pw=this.p.createView();
+		pw.majJoueur(this);
+		pw.decr_nb_indices();
+		if(couleur != null)
+		{
+			for(Carte c : j.getMain().main)
+			{
+				if((!c.isCouleurConnue())&&(c.getCouleur()==couleur))
+				{
+					if(c.isJouableOmniscient(p))
+					{
+						pw.incr_info_jouables();
+					}
+					else if(c.isDefaussableOmniscient(p))
+					{
+						pw.incr_info_defaussables();
+					}
+					else
+					{
+						pw.incr_info_neutres();
+					}	
+				}
+			}
+		}
+		
+		else if(valeur != 0)
+		{
+			for(Carte c : j.getMain().main)
+			{
+				if((!c.isValeurConnue())&&(c.getValeur()==valeur))
+				{
+					if(c.isJouableOmniscient(p))
+					{
+						pw.incr_info_jouables();
+					}
+					else if(c.isDefaussableOmniscient(p))
+					{
+						pw.incr_info_defaussables();
+					}
+					else
+					{
+						pw.incr_info_neutres();
+					}	
+				}
+			}
+		}
+		return pw.valeur_heuristique();
+	}
+}
---
+++ Hanabi/src/model/PartieView.java
@@ -0,0 +1,194 @@
+package model;
+
+import java.util.ArrayList;
+
+import model.Couleur.CardColor;
+
+public class PartieView extends Partie{
+	
+	private int nb_points;
+	private int nb_indices;
+	private int nb_erreurs;
+	private int nb_jouables;
+	private int nb_defaussables;
+	private int info_jouables;
+	private int info_defaussables;
+	private int info_neutres;
+	
+	private double h_nbp=5;
+	private double h_nbi=1.5;
+	private double h_nbe=4;
+	private double h_nbj=1.5;
+	private double h_nbd=1.25;
+	private double h_ij=2;
+	private double h_id=1.6;
+	private double h_in=1.25;
+	
+	public PartieView(Partie p)
+	{
+		/*Partie clone*/
+		super(p.nbJoueurs,p.maxIndices, p.multicolor, true);
+		/*this.jetonEclair = p.jetonEclair;
+		this.jetonIndice=p.jetonIndice;
+		this.aQuiLeTour=p.aQuiLeTour;
+		this.dernierJoueur=p.dernierJoueur;
+		this.dernierTour=p.dernierTour;
+		this.defausse = (ArrayList<Carte>)p.defausse.clone();
+		this.pioche = (ArrayList<Carte>)p.pioche.clone();
+		this.cartesJouees.put(CardColor.BLANC, (ArrayList<Carte>)p.cartesJouees.get(CardColor.BLANC).clone());
+		this.cartesJouees.put(CardColor.BLEU, (ArrayList<Carte>)p.cartesJouees.get(CardColor.BLEU).clone());
+		this.cartesJouees.put(CardColor.VERT, (ArrayList<Carte>)p.cartesJouees.get(CardColor.VERT).clone());
+		this.cartesJouees.put(CardColor.ROUGE, (ArrayList<Carte>)p.cartesJouees.get(CardColor.ROUGE).clone());
+		this.cartesJouees.put(CardColor.JAUNE, (ArrayList<Carte>)p.cartesJouees.get(CardColor.JAUNE).clone());
+		if(this.multicolor){
+			this.cartesJouees.put(CardColor.MULTI, (ArrayList<Carte>)p.cartesJouees.get(CardColor.MULTI).clone());
+		}
+		this.dernierTour = p.dernierTour;
+		this.dernierJoueur = p.dernierJoueur;
+		this.partieFinie = p.partieFinie;
+		this.joueurs=p.joueurs.clone();
+		*/
+		/*PartieView specific*/
+		this.nb_points=p.calculerPoints();
+		this.nb_indices=p.jetonIndice;
+		this.nb_erreurs=p.jetonEclair;
+	}
+	public void majJoueur(JoueurIA j)
+	{
+		this.nb_jouables=j.compterCartesJouables();
+		this.nb_defaussables=j.compterCartesDefaussables();
+		this.info_jouables=j.compterInfosJouables();
+		this.info_defaussables=j.compterInfosDefaussables();
+		this.info_neutres=j.compterInfosNeutres();
+	}
+	
+	public int get_nb_points() {
+		return nb_points;
+	}
+
+
+	public void set_nb_points(int nb_points) {
+		this.nb_points = nb_points;
+	}
+	public void incr_nb_points(){
+		nb_points++;
+	}
+	public void decr_nb_points(){
+		nb_points--;
+	}
+	public int get_nb_indices() {
+		return nb_indices;
+	}
+
+
+	public void set_nb_indices(int nb_indices) {
+		this.nb_indices = nb_indices;
+	}
+	public void incr_nb_indices(){
+		nb_indices++;
+	}
+	public void decr_nb_indices(){
+		nb_indices--;
+	}
+
+	public int get_nb_erreurs() {
+		return nb_erreurs;
+	}
+
+
+	public void set_nb_erreurs(int nb_erreurs) {
+		this.nb_erreurs = nb_erreurs;
+	}
+	public void incr_nb_erreurs(){
+		nb_erreurs++;
+	}
+	public void decr_nb_erreurs(){
+		nb_erreurs--;
+	}
+
+	public int get_nb_jouables() {
+		return nb_jouables;
+	}
+
+
+	public void set_nb_jouables(int nb_jouables) {
+		this.nb_jouables = nb_jouables;
+	}
+	public void incr_nb_jouables(){
+		nb_jouables++;
+	}
+	public void decr_nb_jouables(){
+		nb_jouables--;
+	}
+
+	public int get_nb_defaussables() {
+		return nb_defaussables;
+	}
+
+	public void set_nb_defaussables(int nb_defaussables) {
+		this.nb_defaussables = nb_defaussables;
+	}
+	public void incr_nb_defaussables(){
+		nb_defaussables++;
+	}
+	public void decr_nb_defaussables(){
+		nb_defaussables--;
+	}
+
+	public int get_info_jouables() {
+		return info_jouables;
+	}
+
+
+	public void set_info_jouables(int info_jouables) {
+		this.info_jouables = info_jouables;
+	}
+	public void incr_info_jouables(){
+		info_jouables++;
+	}
+	public void decr_info_jouables(){
+		info_jouables--;
+	}
+
+	public int get_info_defaussables() {
+		return info_defaussables;
+	}
+
+
+	public void set_info_defaussables(int info_defaussables) {
+		this.info_defaussables = info_defaussables;
+	}
+	public void incr_info_defaussables(){
+		info_defaussables++;
+	}
+	public void decr_info_defaussables(){
+		info_defaussables--;
+	}
+
+	public int get_info_neutres() {
+		return info_neutres;
+	}
+
+
+	public void set_info_neutres(int info_neutres) {
+		this.info_neutres = info_neutres;
+	}
+	public void incr_info_neutres(){
+		info_defaussables++;
+	}
+	public void decr_info_neutres(){
+		info_defaussables--;
+	}
+
+	public double valeur_heuristique(){
+		double val=0;
+		if(nb_erreurs==3)
+			val=0;
+		else
+		{
+			val= 	h_nbp*nb_points+h_nbi*nb_indices+h_nbe*nb_erreurs+h_nbj*nb_jouables+h_nbd*nb_defaussables
+					+h_ij*info_jouables+h_id*info_defaussables+h_in*info_neutres;
+		}
+		return val;
+	}
+}
