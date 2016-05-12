396b3c253a95c5b566978258becb6414c9e8b162
BufferOverfl0w <cedric.hubert13@gmail.com>
2016-05-12 11:51:47 +0200

    multicolor_heuristic

--- Hanabi/src/controller/EcouteBouton.java
+++ Hanabi/src/controller/EcouteBouton.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 
 import model.AdditionMainPleineException;
 import model.DummyJoueurIA;
+import model.EpistemicJoueurIA;
 import model.PiocheVideException;
 import model.SemiDummyJoueurIA;
 import model.HeuristicJoueurIA;
@@ -39,9 +40,12 @@ public class EcouteBouton implements ActionListener {
 			    else if(p.joueurIA1.getSelectedItem().equals(SemiDummyJoueurIA.class.getName())){
 			    	joue[1] = new SemiDummyJoueurIA("Sherlock", game, 1);
 			    }
-			    else {
+			    else if(p.joueurIA1.getSelectedItem().equals(HeuristicJoueurIA.class.getName())){
 			    	ParamHeuristic param=new ParamHeuristic();
 			    	joue[1] = new HeuristicJoueurIA("Sherlock", game, 1,param);
+			    }
+			    else{
+			    	joue[1] = new EpistemicJoueurIA("Sherlock", game, 1,0.62);
 			    }
 			    if((Integer)p.nbJoueurs.getSelectedItem()>2){
 				    if(p.joueurIA2.getSelectedItem().equals(DummyJoueurIA.class.getName())){
@@ -50,33 +54,42 @@ public class EcouteBouton implements ActionListener {
 				    else if(p.joueurIA2.getSelectedItem().equals(SemiDummyJoueurIA.class.getName())){
 				    	joue[2] = new SemiDummyJoueurIA("Watson", game, 2);
 				    }
-				    else {
+				    else if(p.joueurIA2.getSelectedItem().equals(HeuristicJoueurIA.class.getName())){
 				    	ParamHeuristic param=new ParamHeuristic();
 				    	joue[2] = new HeuristicJoueurIA("Watson", game, 2,param);
+				    }
+				    else{
+				    	joue[2] = new EpistemicJoueurIA("Watson", game, 2,0.62);
 				    }
 			    }
 			    if((Integer)p.nbJoueurs.getSelectedItem()>3){
 				    if(p.joueurIA3.getSelectedItem().equals(DummyJoueurIA.class.getName())){
 				    	joue[3] = new DummyJoueurIA("Moriarty", game, 3);
 				    }
-				    else if(p.joueurIA2.getSelectedItem().equals(SemiDummyJoueurIA.class.getName())){
+				    else if(p.joueurIA3.getSelectedItem().equals(SemiDummyJoueurIA.class.getName())){
 				    	joue[3] = new SemiDummyJoueurIA("Moriarty", game, 3);
 				    }
-				    else{
+				    else if(p.joueurIA3.getSelectedItem().equals(HeuristicJoueurIA.class.getName())){
 				    	ParamHeuristic param=new ParamHeuristic();
 				    	joue[3] = new HeuristicJoueurIA("Moriarty", game, 3,param);
+				    }
+				    else{
+				    	joue[3] = new EpistemicJoueurIA("Moriarty", game, 3,0.62);
 				    }
 			    }
 			    if((Integer)p.nbJoueurs.getSelectedItem()>4){
 				    if(p.joueurIA4.getSelectedItem().equals(DummyJoueurIA.class.getName())){
 				    	joue[4] = new DummyJoueurIA("Lestrade", game, 4);
 				    }
-				    else if(p.joueurIA2.getSelectedItem().equals(SemiDummyJoueurIA.class.getName())){
+				    else if(p.joueurIA4.getSelectedItem().equals(SemiDummyJoueurIA.class.getName())){
 				    	joue[4] = new SemiDummyJoueurIA("Lestrade", game, 4);
 				    }
-				    else{
+				    else if(p.joueurIA4.getSelectedItem().equals(HeuristicJoueurIA.class.getName())){
 				    	ParamHeuristic param=new ParamHeuristic();
-				    	joue[4] = new HeuristicJoueurIA("Lestrade", game, 4, param);
+				    	joue[4] = new HeuristicJoueurIA("Lestrade", game, 4,param);
+				    }
+				    else{
+				    	joue[4] = new EpistemicJoueurIA("Lestrade", game, 4,0.62);
 				    }
 			    }
 			    try {
